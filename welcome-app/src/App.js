76e61dfa3617d0b83f49f68338c7485bd6a5fc86
uniquesp <sakshisp1342002@gmail.com>
2024-06-23 12:00:57 +0530

    projects

---
+++ welcome-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+         Welcome to React Course on CodingNinjas.com
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
